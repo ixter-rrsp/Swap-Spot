450e2a9bcba8bfcf25118aa56cf834e59cfb8e46
ixter-rrsp <princemuncada28@gmail.com>
2026-07-26 22:53:05 +0800

    Add Vercel Speed Insights

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["192.168.1.3", "precook-configure-swept.ngrok-free.dev"],
+  allowedDevOrigins: ["192.168.1.3", "https://swap-spot-d7b4xu7az-ixter-rrsps-projects.vercel.app"],
   images: {
     remotePatterns: [
       {
